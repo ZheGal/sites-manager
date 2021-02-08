0ecedc268f088d62a2a94ed00fe68a6c40c2ec03
Eugeniy Galkovskiy <zhegal@gmail.com>
2021-02-08 12:37:20 +0200

    https -> http

--- app/Helpers/Settings.php
+++ app/Helpers/Settings.php
@@ -7,7 +7,7 @@ class Settings
     public static function getArray($domain)
     {
         $template = self::getDefaultSettings();
-        $url = 'https://' . $domain . '/settings.json';
+        $url = 'http://' . $domain . '/settings.json';
         $json = @file_get_contents($url);
         if (!empty($json)) {
             $array = json_decode($json, 1);
