8842fc6a82a404c731fd4f6514fcf4d5237fac57
Eugeniy Galkovskiy <zhegal@gmail.com>
2021-03-30 16:12:04 +0300

    geo location

---
+++ app/Http/Controllers/ApiLocation.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Eseath\SxGeo\SxGeo;
+
+class ApiLocation extends Controller
+{
+    public function get_location($ip)
+    {
+        $sxGeo = new SxGeo('../ignored/SxGeoCity.dat');
+        $info  = $sxGeo->getCityFull($ip);
+
+        $result = [];
+        $result['ip'] = $ip;
+        if (isset($info['country'])) {
+            $result['country'] = $info['country']['name_en'];
+            $result['iso'] = $info['country']['iso'];
+        }
+        if (isset($info['city']['name_en'])) {
+            $result['city'] = $info['city']['name_en'];
+        }
+        
+        return response()->json($result);
+    }
+}
